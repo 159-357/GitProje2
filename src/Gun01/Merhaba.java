086c94ef262b183057a23a27e986c905c274c74b
Alp Eren TOKSOY <alperentoksoy95@gmail.com>
2022-10-11 09:34:36 +0300

    ikinci taskımı bitirdim.

--- src/Gun01/Merhaba.java
+++ src/Gun01/Merhaba.java
@@ -2,6 +2,10 @@ package Gun01;
 
 public class Merhaba {
     public static void main(String[] args) {
+        System.out.println("merhaba");
+        for (int i = 0; i < 3; i++) {
+            System.out.println("merhaba"+i);
+        }
 
     }
 }
